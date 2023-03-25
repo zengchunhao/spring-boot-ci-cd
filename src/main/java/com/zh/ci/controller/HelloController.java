3ec755f68ce505e0168e1249086db06e92c70109
曾浩 <zengchunhao@gmail.com>
2023-03-25 21:13:10 +0800

    init commit

---
+++ src/main/java/com/zh/ci/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.zh.ci.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    public String sayHello() {
+        return "hello world";
+    }
+}
